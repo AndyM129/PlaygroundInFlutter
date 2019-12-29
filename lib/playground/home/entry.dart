76eed221bafc5bb6da8d335e4db0afdbe756e22e
mengxinxin <andy_m129@163.com>
2019-12-29 16:10:07 +0800

    feature: 添加 各组件的基础用法示例 的入口

--- lib/playground/home/entry.dart
+++ lib/playground/home/entry.dart
@@ -3,6 +3,11 @@ import 'package:playground_in_flutter/playground/hello_flutter/hello_flutter.dar
 import 'package:playground_in_flutter/playground/times_counter/times_counter_page.dart';
 import 'package:playground_in_flutter/playground/text/text_page.dart';
 import 'package:playground_in_flutter/playground/button/button_page.dart';
+import 'package:playground_in_flutter/playground/image/image_page.dart';
+import 'package:playground_in_flutter/playground/icon/icon_page.dart';
+import 'package:playground_in_flutter/playground/check_box/check_box_page.dart';
+import 'package:playground_in_flutter/playground/text_field/text_field_page.dart';
+import 'package:playground_in_flutter/playground/form/form_page.dart';
 
 /// 实例
 class Entry {
@@ -29,6 +34,26 @@ List<Entry> entries = [
 	Entry(
     title: "Widget - Button",
 		page: ButtonPage(),
+  ),
+	Entry(
+    title: "Widget - Image",
+		page: ImagePage(),
+  ),
+	Entry(
+    title: "Widget - Icon",
+		page: IconPage(),
+  ),
+	Entry(
+    title: "Widget - CheckBox",
+		page: CheckBoxPage(),
+  ),
+	Entry(
+    title: "Widget - TextField",
+		page: TextFieldPage(),
+  ),
+	Entry(
+    title: "Widget - Form",
+		page: FormPage(),
   ),
   Entry(
     title: "···",
